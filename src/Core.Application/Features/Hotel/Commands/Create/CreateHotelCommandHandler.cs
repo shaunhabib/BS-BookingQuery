69527c882abb57e047ca9472b5df9016d7187748
habibur.softifybd <habibur.softifybd@gmail.com>
2023-03-09 22:16:25 +0600

    Hotel booking backed done

---
+++ src/Core.Application/Features/Hotel/Commands/Create/CreateHotelCommandHandler.cs
@@ -0,0 +1,68 @@
+﻿using Core.Application.Contracts.Features.Hotel.Commands.Create;
+using Core.Application.Extensions;
+using Core.Domain.Persistence.Contracts;
+using Core.Domain.Shared.Wrappers;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Core.Application.Features.Hotel.Commands.Create
+{
+    public class CreateHotelCommandHandler : IRequestHandler<CreateHotelCommand, Response<int>>
+    {
+        #region ctor
+        private readonly IPersistenceUnitOfWork _persistenceUnitOfWork;
+        private readonly ILogger<CreateHotelCommandHandler> _logger;
+        private List<String> _validationError;
+
+        public CreateHotelCommandHandler(IPersistenceUnitOfWork persistenceUnitOfWork, ILogger<CreateHotelCommandHandler> logger)
+        {
+            _persistenceUnitOfWork = persistenceUnitOfWork;
+            _logger = logger;
+            _validationError = new List<string>();
+        }
+        #endregion
+        public async Task<Response<int>> Handle(CreateHotelCommand command, CancellationToken cancellationToken)
+        {
+            try
+            {
+                #region checking feature given or not 
+                if (command.Features == null || !command.Features.Any())
+                {
+                    string msg = "Hotel features is required";
+                    _logger.LogError(msg);
+                    return Response<int>.Fail(msg);
+                }
+                #endregion
+
+                var newHotel = new Domain.Persistence.Entities.Hotel
+                {
+                    Name = command.Name,
+                    Description = command.Description,
+                    Address = command.Address,
+                    City = command.City,
+                    State = command.State,
+                    Country = command.Country,
+                    Phone = command.Phone,
+                    Email = command.Email,
+                    Rating = command.Rating,
+                    Features = command.Features == null ? null : command.Features.Select(s => new Domain.Persistence.Entities.HotelFeature
+                    {
+                        Name = s.Name,
+                        Description = s.Description
+                    }).ToList()
+                };
+                await _persistenceUnitOfWork.Hotel.AddAsync(newHotel);
+                await _persistenceUnitOfWork.SaveChangesAsync();
+
+                return Response<int>.Success(newHotel.Id, "Successfully created hotel.");
+                
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.GetFullMessage());
+                _validationError.Add(ex.GetFullMessage());
+                return Response<int>.Fail(_validationError);
+            }
+        }
+    }
+}
---
+++ src/Core.Application/Features/Hotel/Commands/Update/UpdateHotelCommandHandler.cs
@@ -0,0 +1,101 @@
+﻿using Core.Application.Contracts.Features.Hotel.Commands.Update;
+using Core.Application.Extensions;
+using Core.Domain.Persistence.Contracts;
+using Core.Domain.Persistence.Entities;
+using Core.Domain.Shared.Wrappers;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace Core.Application.Features.Hotel.Commands.Update
+{
+    public class UpdateHotelCommandHandler : IRequestHandler<UpdateHotelCommand, Response<bool>>
+    {
+        #region ctor
+        private readonly IPersistenceUnitOfWork _persistenceUnitOfWork;
+        private readonly ILogger<UpdateHotelCommandHandler> _logger;
+        private List<String> _validationError;
+
+        public UpdateHotelCommandHandler(IPersistenceUnitOfWork persistenceUnitOfWork, ILogger<UpdateHotelCommandHandler> logger)
+        {
+            _persistenceUnitOfWork = persistenceUnitOfWork;
+            _logger = logger;
+            _validationError = new List<string>();
+        }
+        #endregion
+        public async Task<Response<bool>> Handle(UpdateHotelCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                #region checking feature given or not 
+                if (request.Features == null || !request.Features.Any())
+                {
+                    string msg = "Hotel features is required";
+                    _logger.LogError(msg);
+                    return Response<bool>.Fail(msg);
+                }
+                #endregion
+
+                var exHotel = _persistenceUnitOfWork.Hotel.AsQueryable()
+                    .Include(i => i.Features)
+                    .FirstOrDefault(x => x.Id == request.Id);
+
+                if (exHotel is null)
+                {
+                    string msg = "No hotel info found to update";
+                    _logger.LogError(msg);
+                    return Response<bool>.Fail(msg);
+                }
+
+                #region update hotel
+                exHotel.Name = request.Name;
+                exHotel.Description = request.Description;
+                exHotel.Address = request.Address;
+                exHotel.City = request.City;
+                exHotel.State = request.State;
+                exHotel.Country = request.Country;
+                exHotel.Phone = request.Phone;
+                exHotel.Email = request.Email;
+                exHotel.Rating = request.Rating;
+
+                #region Features
+                var newFeatures = new List<Domain.Persistence.Entities.HotelFeature>();
+
+                foreach (var feature in request.Features)
+                {
+                    var exFeature = exHotel.Features.FirstOrDefault(x => x.Id == feature.Id);
+                    if (exFeature != null)
+                    {
+                        exFeature.Name = feature.Name;
+                        exFeature.Description = feature.Description;
+                    }
+                    else
+                    {
+                        newFeatures.Add(new Domain.Persistence.Entities.HotelFeature
+                        {
+                            HotelId = exHotel.Id,
+                            Name = feature.Name,
+                            Description = feature.Description
+                        });
+                    }
+                }
+                if (newFeatures.Any())
+                    await _persistenceUnitOfWork.HotelFeature.AddAsync(newFeatures);
+                #endregion
+
+                await _persistenceUnitOfWork.Hotel.UpdateAsync(exHotel);
+                await _persistenceUnitOfWork.SaveChangesAsync();
+                #endregion
+
+                return Response<bool>.Success(true, "Successfully updated hotel.");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.GetFullMessage());
+                _validationError.Add(ex.GetFullMessage());
+                return Response<bool>.Fail(_validationError);
+            }
+        }
+    }
+}
